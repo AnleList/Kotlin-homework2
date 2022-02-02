ff3e8e21bf3f2e6b772171e50149fc25864f454c
Савосин Антон <anle@list.ru>
2022-02-02 14:14:07 +0300

    Убрал лишние вычисления, переименовал переменные

--- src/main/kotlin/ru/netology/homework2/MoneyTransfers.kt
+++ src/main/kotlin/ru/netology/homework2/MoneyTransfers.kt
@@ -2,10 +2,10 @@ package ru.netology.homework2
 
 fun main() {
     val amount = 1_000_00 //в копейках
-    val normalCommissionPart = 0.0075
-    val minimalCommissionValue = 35_00 //в копейках
+    val normalCommission = amount*0.0075
+    val minimalCommission = 35_00 //в копейках
 
-    val commission = if (amount*normalCommissionPart <= minimalCommissionValue) minimalCommissionValue
-        else amount*normalCommissionPart
+    val commission = if (normalCommission <= minimalCommission) minimalCommission else normalCommission
+
     println("коммисия составит: ${commission.toInt()} копеек")
 }
