f8f0ddd60aa4f5593cdaf1511be32e2244de0b1e
Савосин Антон <anle@list.ru>
2022-02-02 16:23:03 +0300

    Выполнена вторая задача

--- src/main/kotlin/ru/netology/peopleOrPeopls/PeopleOrPeopls.kt
+++ src/main/kotlin/ru/netology/peopleOrPeopls/PeopleOrPeopls.kt
@@ -1,2 +1,14 @@
 package ru.netology.peopleOrPeopls
 
+fun main() {
+    val likes = 21
+    val resultWord = if (likes == 0) {
+        "человек"
+    }    else if (!(likes.toString()).endsWith("1") || likes == 11) {
+        "людям"
+    }    else {
+        "человеку"
+    }
+
+    println("Понравилось $likes $resultWord")
+}
